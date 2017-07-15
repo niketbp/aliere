31920b3e8077cd86141177c8c8b67701fc58d4ed
Amy Liu <liuamyj@stanford.edu>
2017-07-15 02:01:07 -0700

    working on fund component

--- frontend/app/components/controllers/FeedController.js
+++ frontend/app/components/controllers/FeedController.js
@@ -5,12 +5,22 @@ angular.module('aliereApp.feed', ['ngRoute'])
 // Controller definition for this module
 .controller('FeedController', ['$scope', function($scope) {
 
-	init();
-
-	function init(){
-
-	};
-
-	this.message = "Hello Home!";
+	$scope.currentStocks = [{
+		name: 'AAPL', 
+		price: 111.06, 
+		change: 0.65,
+		shares: '100 shares'
+	}, {
+		name: 'MSFT', 
+		price: 72.78, 
+		change: 1.01,
+		shares: '50 shares'
+	}, {
+		name: 'SNAP', 
+		price: 15.27, 
+		change: -0.42, 
+		shares: '100 shares'
+	}
+	];
 
 }]);
