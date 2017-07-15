88f99984829129598b0de3681e295ef7c106f409
tylersco <tylersco4@gmail.com>
2017-07-14 21:34:57 -0700

    Add alpha vantage class and method to get current ticker data

--- backend/alpha_vantage.py
+++ backend/alpha_vantage.py
@@ -1,4 +1,33 @@
+import requests
+
+import globals
+
 class AlphaVantage():
 
     def __init__(self):
-        pass
+        self.__BASE_URL = 'https://www.alphavantage.co/query?'
+
+    def get_ticker_data(self, symbol):
+        function = 'function=TIME_SERIES_DAILY'
+        symbol = 'symbol=' + symbol
+        api_key = 'apikey=' + globals.ALPHA_VANTAGE_KEY
+
+        params = '&'.join([function, symbol, api_key])
+
+        url = self.__BASE_URL + params
+
+        r = requests.get(url)
+        content = r.json()
+        most_recent = content['Meta Data']['3. Last Refreshed']
+        most_recent_stock_data = content['Time Series (Daily)'][most_recent]
+
+
+        return {
+            'open': most_recent_stock_data['1. open'],
+            'high': most_recent_stock_data['2. high'],
+            'low': most_recent_stock_data['3. low'],
+            'current': most_recent_stock_data['4. close']
+        }
+
+av = AlphaVantage()
+print(av.get_current_ticker_data('MSFT'))
