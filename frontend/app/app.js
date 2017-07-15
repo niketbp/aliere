7d741b1f648ef4235c0087c9017b99c553410c97
Yicheng Gong <ygongdev@gmail.com>
2017-07-15 03:43:51 -0500

    testing authentication

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -1,10 +1,9 @@
 'use strict';
 
 // Defining Angular app model with all other dependent modules
-let app = angular.module('aliereApp',['ngRoute','aliereApp.home','aliereApp.login']);
+let app = angular.module('aliereApp', ['ngRoute', 'aliereApp.home','aliereApp.login']);
 
 app.config(function($routeProvider) {
-
 	// Declaration of the default route if neither of the controllers
 	// is supporting the request path
 	$routeProvider.otherwise({ redirectTo: '/'});
--- frontend/app/components/controllers/HomeController.js
+++ frontend/app/components/controllers/HomeController.js
@@ -17,11 +17,53 @@ angular.module('aliereApp.home', ['ngRoute'])
 
 // Controller definition for this module
 .controller('HomeController', ['$scope', function($scope) {
-
 	init();
 
-	function init(){
+  function onSuccess(googleUser) {
+  	$scope.name = googleUser.getBasicProfile().getName();
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+  }
 
+  function onFailure(error) {
+    console.log(error);
+  }
+
+  function renderButton() {
+    gapi.signin2.render('my-signin2', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      'onsuccess': onSuccess,
+      'onfailure': onFailure
+    });
+  }
+  window.renderButton = renderButton;
+
+  $scope.signOut = function(googleUser) {
+  	console.log('hi');
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+    	window.localStorage.removeItem("username");
+      console.log('User signed out.');
+    });
+  }
+
+/*	function onSignIn(googleUser) {
+	  var profile = googleUser.getBasicProfile();
+	  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	  //console.log('Name: ' + profile.getName());
+	  console.log('Image URL: ' + profile.getImageUrl());
+	  window.localStorage.setItem("username", profile.getName());
+	  console.log(window.localStorage.getItem("username"));
+	  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+	};*/
+
+	/*window.onSignIn = onSignIn;*/
+
+	function init(){
+		console.log('hey');
 	};
 
 	this.message = "Hello Home!";
