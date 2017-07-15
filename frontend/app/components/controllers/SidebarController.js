41c35314713e684bb535049dd1fd2c464ab19498
Richard Li <rl606@scarletmail.rutgers.edu>
2017-07-15 09:51:37 -0700

    working join

--- frontend/app/components/controllers/SidebarController.js
+++ frontend/app/components/controllers/SidebarController.js
@@ -15,12 +15,15 @@ angular.module('aliereApp.sidebar', ['ngRoute'])
 
 	console.log(window.localStorage.getItem('username'));
 
+	let tmp = null;
+
 	function getUserData() {
 		$http( {
 			method: 'GET',
 			url: 'http://127.0.0.1:5000/user/data?user=' + window.localStorage.getItem('username')
 		}).then(function success(response) {
 			$scope.userData = response;
+			tmp = response;
 			console.log(response);
 			console.log(response.data.playerFunds[0].fundName);
 		}, function error(response) {
@@ -56,17 +59,31 @@ angular.module('aliereApp.sidebar', ['ngRoute'])
 			method: 'GET',
 			url: 'http://127.0.0.1:5000/fund/all'
 		}).then(function success(response) {
+			$scope.openFunds = [];
 			console.log(response);
+			response.data.forEach(function(element1) {
+				let flag = 0;
+				$scope.userData.data.playerFunds.forEach(function(element2) {
+					if(element1.fundName == element2.fundName) {
+						flag = 1;
+					}
+				})
+				if(flag == 0) {
+					$scope.openFunds.push(element1);
+				}
+			});
+			console.log($scope.openFunds);
 		}, function error(response) {
 			console.log(response);
 		});
 	}
 
-	$scope.joinFund = function() {
+	$scope.joinFund = function(name) {
 		$http({
 			method: 'GET',
-			url: 'http://127.0.0.1:5000/fund/join?name=Voyager&username=' + window.localStorage.getItem('username')
+			url: 'http://127.0.0.1:5000/fund/join?name=' + name + '&username=' + window.localStorage.getItem('username')
 		}).then(function success(response) {
+			$scope.reload;
 			console.log(response);
 		}, function error(response) {
 			console.log(response);
